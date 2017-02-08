dd671daf592a4f063b50e964b6976274f7418bff
Bill Gray <wgray01@wgray.com>
2017-02-08 17:40:16 -0500

    commits

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var app = express();
 //Configure express to deliver static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, resp){
-    res.sendFile('index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index1.html');
 });
 
 module.exports = app;
